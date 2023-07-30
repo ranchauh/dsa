cc46b2ca9fe584d4a617779d2364ebbf5dde0584
Randheer Chauhan <randheerschauhan@gmail.com>
2023-07-30 08:03:45 +0530

    tree

---
+++ src/main/tree/BuildTree.java
@@ -0,0 +1,67 @@
+package tree;
+
+import java.util.ArrayList;
+
+public class BuildTree {
+
+    public TreeNode buildTreeWithInAndPost(ArrayList<Integer> A, ArrayList<Integer> B) {
+        if(A.size() != B.size()) {
+            return null;
+        }
+        return buildTreeWithInAndPost(B, A, 0, B.size()-1, 0, A.size()-1);
+    }
+
+    private TreeNode buildTreeWithInAndPost(ArrayList<Integer> postorder, ArrayList<Integer> inorder, int ps, int pe, int is, int ie) {
+        if(is > ie || ps > pe) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(postorder.get(pe));
+        int rootIdx = search(inorder, is, ie, root.val);
+        int elInLst = rootIdx - is;
+        // build left subtree
+        root.left = buildTreeWithInAndPost(postorder, inorder, ps, ps + elInLst - 1 , is, rootIdx-1);
+
+        // build right subtree
+        root.right = buildTreeWithInAndPost(postorder, inorder, ps + elInLst , pe - 1, rootIdx+1, ie);
+
+        return root;
+    }
+
+    public TreeNode buildTreeWithInAndPre(ArrayList<Integer> A, ArrayList<Integer> B) {
+        if(A.size() != B.size()) {
+            return null;
+        }
+        return buildTreeWithInAndPre(A, B, 0, A.size()-1, 0, B.size()-1);
+    }
+
+    private TreeNode buildTreeWithInAndPre(ArrayList<Integer> preorder, ArrayList<Integer> inorder, int ps, int pe, int is, int ie) {
+        if(is > ie || ps > pe) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(preorder.get(ps));
+        int rootIdx = search(inorder, is, ie, root.val);
+        int elInLst = rootIdx - is;
+        // build left subtree
+        root.left = buildTreeWithInAndPre(preorder, inorder, ps+1, ps + elInLst, is, rootIdx-1);
+
+        // build right subtree
+        root.right = buildTreeWithInAndPre(preorder, inorder, ps+elInLst+1, pe, rootIdx+1, ie);
+
+        return root;
+    }
+
+    private int search(ArrayList<Integer> inorder, int s, int e, int val) {
+        for(int i=s; i<=e; i++) {
+            if(inorder.get(i) == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/tree/TreeTraversal.java
+++ src/main/tree/TreeTraversal.java
@@ -1,7 +1,27 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+class Pair {
+    /**
+     * TreeNode
+     */
+    TreeNode node;
+    /**
+     Task Definition:
+     1: insert left child to stack.
+     2: insert right child to stack.
+     3: insert node value to result
+     4: pop from the stack
+     */
+    int task;
+    Pair(TreeNode node, int task) {
+        this.node = node;
+        this.task = task;
+    }
+}
 public class TreeTraversal {
     public void inorder(TreeNode root, ArrayList<Integer> output) {
         if(root == null) {
@@ -12,6 +32,26 @@ public class TreeTraversal {
         output.add(root.val);
 
         inorder(root.right, output);
+    }
+
+    public List<Integer> inorderIterative(TreeNode A) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        TreeNode root = A;
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            while(root != null && root.left != null) {
+                root = root.left;
+                stack.push(root);
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+            if(root != null) {
+                stack.push(root);
+            }
+        }
+        return list;
     }
 
     public void preorder(TreeNode root, ArrayList<Integer> output) {
@@ -25,6 +65,23 @@ public class TreeTraversal {
 
         preorder(root.right, output);
     }
+    public ArrayList<Integer> preorderIterative(TreeNode A) {
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        TreeNode root = A;
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            result.add(root.val);
+            if(root.right != null) {
+                stack.push(root.right);
+            }
+            if(root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return result;
+    }
 
     public void postorder(TreeNode root, ArrayList<Integer> output) {
         if(root == null) {
@@ -36,6 +93,57 @@ public class TreeTraversal {
         postorder(root.right, output);
 
         output.add(root.val);
+    }
+
+    public List<Integer> postorderIterativeWithPair(TreeNode A) {
+        Stack<Pair> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        Pair pair = new Pair(A, 1);
+        stack.push(pair);
+        while(!stack.isEmpty()) {
+            pair = stack.peek();
+            TreeNode node = pair.node;
+            if(pair.task == 1) {
+                if(node.left != null) {
+                    stack.push(new Pair(node.left, 1));
+                }
+                pair.task++;
+            } else if(pair.task == 2) {
+                if(node.right != null) {
+                    stack.push(new Pair(node.right, 1));
+                }
+                pair.task++;
+            } else if(pair.task == 3) {
+                result.add(node.val);
+                pair.task++;
+            } else {
+                stack.pop();
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> postorderIterativeWith(TreeNode A) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        stack1.push(A);
+        while(!stack1.isEmpty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+            if(node.left != null) {
+                stack1.push(node.left);
+            }
+            if(node.right != null) {
+                stack1.push(node.right);
+            }
+        }
+
+        while(!stack2.isEmpty()) {
+            result.add(stack2.pop().val);
+        }
+
+        return result;
     }
 
     public int countNodes(TreeNode root) {
