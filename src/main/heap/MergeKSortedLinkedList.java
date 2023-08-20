210fe9bbd525409c21d6c0a1be5535e3e6419a5a
Randheer Chauhan <randheerschauhan@gmail.com>
2023-08-20 12:43:14 +0530

    heap

--- src/main/heap/MergeKSortedLinkedList.java
+++ src/main/heap/MergeKSortedLinkedList.java
@@ -40,11 +40,54 @@ public class MergeKSortedLinkedList {
         return head;
     }
 
+    public static ListNode mergeKListsUsingTreeMap(List<ListNode> A) {
+        TreeMap<Integer, ArrayList<ListNode>> map = new TreeMap<>();
+        ListNode node;
+        ArrayList<ListNode> list;
+        int val;
+        for (ListNode head : A) {
+            node = head;
+            while (node != null) {
+                val = node.val;
+
+                if (map.containsKey(val)) {
+                    list = map.get(val);
+                    list.add(node);
+                } else {
+                    list = new ArrayList<>();
+                    list.add(node);
+                    map.put(val, list);
+                }
+
+                node = node.next;
+            }
+        }
+        ListNode head = null;
+        node = null;
+
+        for (Map.Entry<Integer, ArrayList<ListNode>> entry : map.entrySet()) {
+            list = entry.getValue();
+            for (ListNode n : list) {
+                if (head == null)
+                    head = n;
+                if (node != null)
+                    node.next = n;
+                node = n;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         ListNode h1 = LinkedList.arrayToLinkedList(new int[]{1,10,20});
         ListNode h2 = LinkedList.arrayToLinkedList(new int[]{4,11,13});
         ListNode h3 = LinkedList.arrayToLinkedList(new int[]{3,8,9});
-        ListNode head = mergeKLists(Arrays.asList(h1, h2, h3));
+        ListNode head = mergeKListsUsingTreeMap(Arrays.asList(h1, h2, h3));
         LinkedList.print(head); // 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
+        h1 = LinkedList.arrayToLinkedList(new int[]{1,10,20});
+        h2 = LinkedList.arrayToLinkedList(new int[]{4,11,13});
+        h3 = LinkedList.arrayToLinkedList(new int[]{3,8,9});
+        ListNode head2 = mergeKLists(Arrays.asList(h1, h2, h3));
+        LinkedList.print(head2); // 1 -> 3 -> 4 -> 8 -> 9 -> 10 -> 11 -> 13 -> 20
     }
 }
